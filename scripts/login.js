4e4af2394962b98d6ebf356397cc555466020dc7
emon-101 <emon26.ugv@gmail.com>
2026-03-07 21:03:34 +0600

    write the logic for login home page

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+document.getElementById('login-btn').addEventListener('click', ()=> {
+    // get the username
+    const userInputField = document.getElementById('username-input');
+    const userInputValue = userInputField.value;
+    // get the user password
+    const userPasswordField = document.getElementById('password-input');
+    const userPasswordValue = userPasswordField.value;
+    
+    // Credential check
+    if(userInputValue === "admin" && userPasswordValue === "admin123") {
+        window.location.assign('/index.html');
+    } else {
+        alert('Please provide the correct information');
+        return;
+    }
+})
